740267d8e061f06ec4018bed8a0c0e063143773a
Yves Bode <ynrbode@gmail.com>
2020-11-18 19:28:02 -0300

    🚧 Implemented basic functionality in Dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,71 +1,68 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import { Title, Form, Repositories } from './styles';
+import api from '../../services/api';
 import appLogoImg from '../../assets/app-logo.svg';
 import arrowImg from '../../assets/arrow.svg';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  async function handleSubmit(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+    const response = await api.get<Repository>(`/repos/${searchQuery}`);
+    setRepositories([...repositories, response.data]);
+  }
+
   return (
     <>
       <img src={appLogoImg} alt="GitHub Explorer logo" />
 
       <Title>Explore GitHub repositories.</Title>
 
-      <Form>
-        <input type="text" name="search" placeholder="Type Here" />
-        <button type="button">Search</button>
+      <Form onSubmit={handleSubmit}>
+        <input
+          onChange={e => setSearchQuery(e.target.value)}
+          type="text"
+          name="search"
+          placeholder="Type Here"
+        />
+        <button type="submit">Search</button>
       </Form>
 
       <Repositories>
-        <a href="https://github.com/Rocketseat/unform">
-          <img
-            className="avatar-img"
-            src="https://picsum.photos/200"
-            alt="Nome Sobrenome"
-          />
-          <div>
-            <h5>Rocketseat/unform</h5>
-            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
-          </div>
-          <img
-            className="right-arrow"
-            src={arrowImg}
-            alt="Right facing arrow"
-          />
-        </a>
-
-        <a href="https://github.com/Rocketseat/unform">
-          <img
-            className="avatar-img"
-            src="https://picsum.photos/200"
-            alt="Nome Sobrenome"
-          />
-          <div>
-            <h5>Rocketseat/unform</h5>
-            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
-          </div>
-          <img
-            className="right-arrow"
-            src={arrowImg}
-            alt="Right facing arrow"
-          />
-        </a>
-
-        <a href="https://github.com/Rocketseat/unform">
-          <img
-            className="avatar-img"
-            src="https://picsum.photos/200"
-            alt="Nome Sobrenome"
-          />
-          <div>
-            <h5>Rocketseat/unform</h5>
-            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
-          </div>
-          <img
-            className="right-arrow"
-            src={arrowImg}
-            alt="Right facing arrow"
-          />
-        </a>
+        {repositories.map(repository => (
+          <a
+            key={repository.full_name}
+            href={`https://github.com/${repository.full_name}`}
+          >
+            <img
+              className="avatar-img"
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <h5>{repository.full_name}</h5>
+              <p>{repository.description}</p>
+            </div>
+            <img
+              className="right-arrow"
+              src={arrowImg}
+              alt="Right facing arrow"
+            />
+          </a>
+        ))}
       </Repositories>
     </>
   );
