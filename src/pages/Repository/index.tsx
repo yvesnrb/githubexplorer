fded9177dda88d7e2c039041e76298294097512d
Yves Bode <ynrbode@gmail.com>
2020-11-19 11:29:39 -0300

    🚧 Implemented route paramethers in Repository

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
+
+interface RouteParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RouteParams>();
+
+  return (
+    <>
+      <h1>Repository</h1>
+      <p>{params.repository}</p>
+    </>
+  );
 };
 
 export default Repository;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,7 +7,7 @@ const Routes: React.FC = () => {
   return (
     <Switch>
       <Route exact path="/" component={Dashboard} />
-      <Route exact path="/repository" component={Repository} />
+      <Route exact path="/repository/:repository+" component={Repository} />
     </Switch>
   );
 };
